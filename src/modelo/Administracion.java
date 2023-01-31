42a4131283558407bd8a4fdb9f9e56346a26505d
JessicaAl28 <124017765+JessicaAl28@users.noreply.github.com>
2023-01-30 23:24:24 -0500

    Create Administracion.java
    Materias de Administracion

---
+++ src/modelo/Administracion.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public class Administracion extends Estudiante{
+    public Administracion() {
+        super("Negocios, Administracion I, Historia de la Administracion");
+    }
+}
